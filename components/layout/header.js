ae16dce3eb93df9a1f03b6441d032de74cb9ba73
aliezaheer <alizaheer67@gmail.com>
2022-11-11 22:07:44 +0500

    social links and nav

--- components/layout/header.js
+++ components/layout/header.js
@@ -2,8 +2,19 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <img src="indie-logo.png" alt="" />
+    <div className="flex mt-5 justify-around align-middle">
+      <img className="w-24" src="indie-logo.png" alt="" />
+      <ul className="flex space-x-4 py-6">
+        <li>Home</li>
+        <li>About</li>
+        <li>desings</li>
+        <li>Contact</li>
+      </ul>
+
+      <div className="flex space-x-2 align-middle py-7">
+        <img className="w-4 h-4" src="instagram.png" alt="" />
+        <img className="w-4 h-4" src="facebook.png" alt="" />
+      </div>
     </div>
   );
 };

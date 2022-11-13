683f44a9310ec194fc7a02b89a88ebee7b0a0f79
aliezaheer <alizaheer67@gmail.com>
2022-11-13 23:35:33 +0500

    photos magnigier

--- components/album/Album.js
+++ components/album/Album.js
@@ -1,39 +1,108 @@
-import React from "react";
+import Head from "next/head";
+
+$(document).ready(function () {
+  $(".image-popup-vertical-fit").magnificPopup({
+    type: "image",
+    mainClass: "mfp-with-zoom",
+    gallery: {
+      enabled: true,
+    },
+
+    zoom: {
+      enabled: true,
+
+      duration: 300, // duration of the effect, in milliseconds
+      easing: "ease-in-out", // CSS transition easing function
+
+      opener: function (openerElement) {
+        return openerElement.is("img")
+          ? openerElement
+          : openerElement.find("img");
+      },
+    },
+  });
+});
 
 const Album = () => {
   return (
     <>
-      <ul class="album">
-        <li class="item">
-          <img src="https://picsum.photos/600/600/?random=1" />
-          <div class="txt">
-            <h3>title1</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-        </li>
-        <li class="item">
-          <img src="https://picsum.photos/600/600/?random=2" />
-          <div class="txt">
-            <h3>title2</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-        </li>
-        <li class="item">
-          <img src="https://picsum.photos/600/600/?random=3" />
-          <div class="txt">
-            <h3>title3</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-        </li>
-        /
-        <li class="item">
-          <img src="https://picsum.photos/600/600/?random=14" />
-          <div class="txt">
-            <h3>title14</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-        </li>
-      </ul>
+      <Head>
+        <title>Indie Hands - Home</title>
+        <meta
+          name="description"
+          content="Best mehndi in the karachi with beautiful desings."
+        />
+        <link rel="icon" href="/mehndi.png" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+      </Head>
+      <div class="container">
+        <p>
+          <section class="img-gallery-magnific">
+            <div class="magnific-img">
+              <a
+                class="image-popup-vertical-fit"
+                href="/photo-1.jpg"
+                title="9.jpg"
+              >
+                <img src="/photo-1.jpg" alt="9.jpg" />
+                <i class="fa fa-search-plus" aria-hidden="true"></i>
+              </a>
+            </div>
+            <div class="magnific-img">
+              <a
+                class="image-popup-vertical-fit"
+                href="https://unsplash.it/974/?random"
+                title="9.jpg"
+              >
+                <img src="https://unsplash.it/974/?random" alt="9.jpg" />
+                <i class="fa fa-search-plus" aria-hidden="true"></i>
+              </a>
+            </div>
+            <div class="magnific-img">
+              <a
+                class="image-popup-vertical-fit"
+                href="https://unsplash.it/974/?random"
+                title="9.jpg"
+              >
+                <img src="https://unsplash.it/974/?random" alt="9.jpg" />
+                <i class="fa fa-search-plus" aria-hidden="true"></i>
+              </a>
+            </div>
+            <div class="magnific-img">
+              <a
+                class="image-popup-vertical-fit"
+                href="https://unsplash.it/900/?random"
+                title="10.jpg"
+              >
+                <img src="https://unsplash.it/900/?random" alt="10.jpg" />
+                <i class="fa fa-search-plus" aria-hidden="true"></i>
+              </a>
+            </div>
+            <div class="magnific-img">
+              <a
+                class="image-popup-vertical-fit"
+                href="https://unsplash.it/902"
+                title="3.jpg"
+              >
+                <img src="https://unsplash.it/902/" alt="3.jpg" />
+                <i class="fa fa-search-plus" aria-hidden="true"></i>
+              </a>
+            </div>
+            <div class="magnific-img">
+              <a
+                class="image-popup-vertical-fit"
+                href="https://unsplash.it/901"
+                title="4.jpg"
+              >
+                <img src="https://unsplash.it/901" alt="4.jpg" />
+                <i class="fa fa-search-plus" aria-hidden="true"></i>
+              </a>
+            </div>
+          </section>
+          <div class="clear"></div>
+        </p>
+      </div>
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
+
 import Hero from "../components/heroSection/Hero";
 import Header from "../components/layout/Header";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
